f63a84fb99e68985d74bdb246f75cb2c83f984e2
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-30 17:31:59 -0700

    Concatanation - str vs int

---
+++ Mangala.py
@@ -0,0 +1,18 @@
+print("Do what u love")
+print("Love what u do")
+name = "Mangala"
+print(type(name))
+print("Hello " + name)
+
+
+first_name = "Mangala"
+last_name = "Karunarathne"
+full_name = first_name + " " +last_name
+print("Hello " + full_name)
+
+age = 27
+# age = age + 2
+age += 2
+print(age)
+print(type(age))
+print("Your age is : "+ str(age))
