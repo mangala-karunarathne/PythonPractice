b791af8f0513c606f332566a6b27ddde1ac39d33
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-30 19:56:41 -0700

    Multiple Assignments

--- Mangala.py
+++ Mangala.py
@@ -1,5 +1,9 @@
 print("Do what u love")
 print("Love what u do")
+
+# -------- Variable Types ------------
+# ----- 1. String / str----------
+
 name = "Mangala"
 print(type(name))
 print("Hello " + name)
@@ -10,9 +14,26 @@ last_name = "Karunarathne"
 full_name = first_name + " " +last_name
 print("Hello " + full_name)
 
+
+# ----- 2. Integers / int ----------
+
 age = 27
 # age = age + 2
 age += 2
 print(age)
 print(type(age))
 print("Your age is : "+ str(age))
+
+
+# ----- 3. Floating Numbers / float ----------
+
+Height = 250.6
+print("Your Height is : " + str(Height) +"cm")
+print(type(Height))
+
+
+# ----- 4. boolean / bool----------
+
+Human = True
+print("Are you a human : "+str(Human))
+print(type(Human))
---
+++ Mangala1.py
@@ -0,0 +1,15 @@
+# Multiple Assignment
+
+name = "Mangala"
+age = 28
+attractive = True
+
+print(name)
+print(age)
+print(attractive)
+
+name, age, attractive = "Rana", 27, False
+
+print(name)
+print(age)
+print(attractive)
