091131059dd1828dcbeedbbe9cdb46cc617b86b6
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-31 12:55:09 -0700

    Multiple Assignments II

--- Mangala1.py
+++ Mangala1.py
@@ -1,5 +1,7 @@
 # Multiple Assignment
 
+# ================== Part 1 ======================
+
 name = "Mangala"
 age = 28
 attractive = True
@@ -13,3 +15,24 @@ name, age, attractive = "Rana", 27, False
 print(name)
 print(age)
 print(attractive)
+
+
+# ================== Part 2 ======================
+
+
+kasun = 30
+nadun = 45
+dasun = 50
+nipun = 55
+
+print(kasun)
+print(nadun)
+print(dasun)
+print(nipun)
+
+kasun = nadun = dasun = nipun = 75
+
+print(kasun)
+print(nadun)
+print(dasun)
+print(nipun)
