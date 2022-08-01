d1388f8c209ebe011274a55c048aad56e2e41258
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-31 19:36:50 -0700

    Useful String Methods

---
+++ Mangala2.py
@@ -0,0 +1,12 @@
+name = 'mangala'
+names = "mangala karunarathne "
+
+print(len(name))
+print(name.find("a"))
+print(name.capitalize())
+print(names.lower())
+print(names.isdigit())
+print(name.isalpha())
+print(names.isalpha())
+print(name.replace("a", "o"))
+print(names*3)
