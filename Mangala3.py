492fe9e86c3caf9798cb06416c5144c0d9fd2441
mangala-karunarathne <mangalaicc@gmail.com>
2022-08-10 22:21:33 -0700

    Type casting ||

---
+++ Mangala3.py
@@ -0,0 +1,16 @@
+# Type casting = convert one data type into another data type
+
+x = 1       #int
+y = 2.0     #floating
+z = "abc"   #str
+
+print(x)
+print(y)
+print(z)
+
+y = int(y)
+print(y)
+
+print("X is "+str(x)) #Concatanate with string
+print("X is "+str(y)) #Concatanate with string
+print("X is "+z) #Concatanate with string
---
+++ Mangala4.py
@@ -0,0 +1,15 @@
+# Type casting Further
+
+name = input("What is your Name : ")
+
+age = int(input("What is your age: "))
+
+height = float(input("How tall are you : "))
+
+age = age + 1
+
+print(" Hello "+name)
+print(" You are " + str(age) + " years old")
+print(" You are " + str(height) + " height")
+
+
